e67a1a34b8b66697c256458571096f8ca999c617
GillianMC <70141395+SandeMC@users.noreply.github.com>
2024-01-24 17:14:45 +0200

    ver change

--- Common/DisplayInfoGrabber.cs
+++ Common/DisplayInfoGrabber.cs
@@ -55,12 +55,36 @@ namespace GTAIVSetupUtilityWPF.Common
         {
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
             public string dmDeviceName;
+            public short dmSpecVersion;
+            public short dmDriverVersion;
             public short dmSize;
+            public short dmDriverExtra;
+            public int dmFields;
+            public int dmPositionX;
+            public int dmPositionY;
+            public int dmDisplayOrientation;
+            public int dmDisplayFixedOutput;
+            public short dmColor;
+            public short dmDuplex;
+            public short dmYResolution;
+            public short dmTTOption;
+            public short dmCollate;
             [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
             public string dmFormName;
+            public short dmLogPixels;
+            public int dmBitsPerPel;
             public int dmPelsWidth;
             public int dmPelsHeight;
+            public int dmDisplayFlags;
             public int dmDisplayFrequency;
+            public int dmICMMethod;
+            public int dmICMIntent;
+            public int dmMediaType;
+            public int dmDitherType;
+            public int dmReserved1;
+            public int dmReserved2;
+            public int dmPanningWidth;
+            public int dmPanningHeight;
         }
 
         [DllImport("user32.dll")]
